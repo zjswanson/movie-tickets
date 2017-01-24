dc47dcd7a50d8f0e5925563e9e21d30ed941eef5
Matt Kelley and Zach Swanson <zach@>
2017-01-24 10:08:10 -0800

    add event handler for form submit

--- js/scripts.js
+++ js/scripts.js
@@ -10,24 +10,46 @@ function Ticket(movieName,movieTime,age) {
 };
 
 // Price formula: Set base price from first/second run, age and show time modify price.  First run = $10  Second run = $8.  Matinee is $2 off, Seniors 10% off, children 20% off.
-var Beaches = new Movie("Beaches","1st");
-var testTicket = new Ticket (Beaches,1100,"senior");
 
-Ticket.prototype.getPrice(){
+Ticket.prototype.getPrice = function() {
   var price= this.movie.basePrice;
   if (this.movieTime < 1600) {
     price = price - 2;
   }
   if (this.age === "senior") {
     price = price*.9;
-  } if else (this.age === "child") {
+  }else if(this.age === "child") {
     price = price *.8;
   }
   return price;
 };
 
+var getMovie = function(title){
+  var movie;
+  if (title === "starWars") {
+    movie = new Movie("Star Wars",10);
+    return movie;
+  } else if (title === "beaches") {
+    movie = new Movie("Beaches",8);
+    return movie;
+  } else {
+    movie = new Movie("Blade Runner",8);
+    return movie;
+  }
+};
 
 $(document).ready(function() {
-  
+  $("form#ticketForm").submit(function(event){
+    event.preventDefault();
+    var chooseMovie = $("select#movie").val();
+    chooseMovie = getMovie(chooseMovie);
+    var chooseTime = $("select#time").val();
+    var chooseAge = $("select#age").val();
+    var ticket = new Ticket (chooseMovie,chooseTime,chooseAge);
+    console.log(ticket);
+    console.log(ticket.getPrice());
+
+
+  });
 
 });
